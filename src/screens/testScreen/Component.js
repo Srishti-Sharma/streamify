de0fd0bac7a10b08dfaba261c0fe635c9a6c38d5
Akash Dey <akashd@geekyants.com>
2021-08-15 20:16:13 +0530

    Adds:() Basic Button Component

--- src/screens/testScreen/Component.js
+++ src/screens/testScreen/Component.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
+import AppButton from '../../components/AppButton';
 const Test = ({test, testAction}) => (
-  <View>
-    <Button
+  <View
+    style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#10222D',
+    }}>
+    <AppButton
       onPress={() => {
         testAction(5);
       }}
       title="Click"
-      color="#841584"
-      accessibilityLabel="Learn more about this purple button"
     />
-    <Text>{test}</Text>
+    <Text style={{color: '#fff', marginTop: 15}}>{test}</Text>
   </View>
 );
 
