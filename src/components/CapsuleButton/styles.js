421f5455aa4deb17007630777e455c3c0e62d98a
Akash Dey <akashd@geekyants.com>
2021-08-25 12:12:55 +0530

    Adds:() Capsule Button Component

---
+++ src/components/CapsuleButton/styles.js
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+import {colorObj} from '../../../assets/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  countContainer: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  linearGradient: props => ({
+    padding: 15,
+    borderRadius: 50,
+    width: props.width,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }),
+  text: {
+    color: colorObj.primaryText,
+    fontWeight: 'bold',
+  },
+});
+export default styles;
